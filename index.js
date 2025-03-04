85d4146fb754abbd7275d5fcb29a79cdd04881cf
SUBHAJIT555 <subhajitdhali0@gmail.com>
2025-03-04 14:41:38 +0400

    nothing

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", function(req, res){
     
 })
 
-
+//----edit routes-----
 app.get("/edit/:filename", function (req, res) {
   res.render("edit",{filename: req.params.filename});
 });
@@ -37,7 +37,7 @@ app.post("/edit", function (req, res) {
 });
 
 
-
+//-------create routes-----
 app.get("/file/:filename", function (req, res) {
   fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
     res.render("show", {filename: req.params.filename, filedata: filedata});
